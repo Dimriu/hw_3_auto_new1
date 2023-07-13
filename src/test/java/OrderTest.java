f0359c3e19f183ff53c89e489b58fbf1b80b7bb2
Your Name <you@example.com>
2023-07-13 09:46:51 +0300

    setting WebDriver

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,9 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OrderTest {
     private WebDriver driver;
 
+//    @BeforeAll
+//    static void setUpAll() {
+//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+//    }
+
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
 
